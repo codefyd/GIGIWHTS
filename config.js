9d673157ac358e471a5c64b5f66128994240a1a9
codefyd <ahsnsy77@gmail.com>
2026-04-17 20:41:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://uqzauvennlrcqoqnvthy.supabase.co",
+  SUPABASE_URL: "https://xbhsdweqlaksnchmjcwj.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_Spzme0_9-eQzY5t6Xnbfqg_lHEU1trl",
-  FUNCTIONS_BASE_URL: "https://uqzauvennlrcqoqnvthy.supabase.co/functions/v1",
+  FUNCTIONS_BASE_URL: "https://xbhsdweqlaksnchmjcwj.supabase.co/functions/v1",
   APP_NAME: "لوحة واتساب أعمال",
   APP_VERSION: "1.0.0",
   DEFAULT_LANGUAGE: "ar",
